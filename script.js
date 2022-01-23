aa02e153235500475fbd6ab0e6cbb968427a6ae7
Igwe0001 <nwobodoemmanuel479@gmail.com>
2022-01-23 11:54:13 +0100

    made changes

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const Hamburger = document.querySelector(".hamburger");
 const Nav = document.querySelector('nav');
 const Line = document.querySelectorAll('.line')
 
+const navbar = document.querySelector('header');
+window.onscroll = () => {
+    if (window.scrollY > 20) {
+        navbar.classList.add('nav-active');
+    } else {
+        navbar.classList.remove('nav-active');
+    }
+};
+
+
 Hamburger.addEventListener("click", function () {
     Nav.classList.toggle("show-links");
    
