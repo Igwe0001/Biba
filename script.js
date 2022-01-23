c2ebb59cc63f5bf9b3f1bd2ef5d002113bb0fd0a
Igwe0001 <nwobodoemmanuel479@gmail.com>
2022-01-23 10:15:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const Hamburger = document.querySelector(".hamburger");
+const Nav = document.querySelector('nav');
+const Line = document.querySelectorAll('.line')
+
+Hamburger.addEventListener("click", function () {
+    Nav.classList.toggle("show-links");
+   
+    
+});
+
+console.log(Line)
+
+
+
